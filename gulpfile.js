e6608f6e6256af9cfcdbfe1ac0c1ff6ba73ad069
Kliment Mamykin <kmamyk@gmail.com>
2014-09-23 01:37:29 -0400

    Building css with scss and putoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,49 @@ var gulp = require('gulp'),
 
 // JSHint task
 gulp.task('lint', function() {
-  gulp.src('./app/scripts/*.js')
-      .pipe(jshint())
-    // You can look into pretty reporters as well, but that's another story
-      .pipe(jshint.reporter('default'));
+    gulp.src('./app/scripts/*.js')
+        .pipe(jshint())
+        // You can look into pretty reporters as well, but that's another story
+        .pipe(jshint.reporter('default'));
 });
 
 // Browserify task
 gulp.task('browserify', function() {
-  // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
-  gulp.src(['app/scripts/main.js'])
-      .pipe(browserify({
-        insertGlobals: true,
-        debug: true
-      }))
-    // Bundle to a single file
-      .pipe(concat('bundle.js'))
-    // Output it to our dist folder
-      .pipe(gulp.dest('dist/js'));
+    // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
+    gulp.src(['app/scripts/main.js'])
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true
+        }))
+        // Bundle to a single file
+        .pipe(concat('bundle.js'))
+        // Output it to our dist folder
+        .pipe(gulp.dest('dist/js'));
 });
 gulp.task('watch', ['lint'], function() {
-  // Watch our scripts
-  gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js'], [
-    'lint',
-    'browserify'
-  ]);
+    // Watch our scripts
+    gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js'], [
+        'lint',
+        'browserify'
+    ]);
 });
 
 // Views task
 gulp.task('views', function() {
-  // Get our index.html
-  gulp.src('app/index.html')
-    // And put it in the dist folder
-      .pipe(gulp.dest('dist/'))
-      .pipe(refresh(lrserver)); // Tell the lrserver to refresh
+    // Get our index.html
+    gulp.src('app/index.html')
+        // And put it in the dist folder
+        .pipe(gulp.dest('dist/'))
+        .pipe(refresh(lrserver)); // Tell the lrserver to refresh
 
-  // Any other view files from app/views
-  gulp.src('./app/views/**/*')
-    // Will be put in the dist/views folder
-      .pipe(gulp.dest('dist/views/'))
-      .pipe(refresh(lrserver)); // Tell the lrserver to refresh
+    // Any other view files from app/views
+    gulp.src('./app/views/**/*')
+        // Will be put in the dist/views folder
+        .pipe(gulp.dest('dist/views/'))
+        .pipe(refresh(lrserver)); // Tell the lrserver to refresh
 });
 gulp.watch(['app/index.html', 'app/views/**/*.html'], [
-  'views'
+    'views'
 ]);
 
 var embedlr = require('gulp-embedlr'),
@@ -68,32 +68,33 @@ server.use(livereload({port: livereloadport}));
 server.use(express.static('./dist'));
 // Because I like HTML5 pushstate .. this redirects everything back to our index.html
 server.all('/*', function(req, res) {
-  res.sendFile('index.html', { root: 'dist' });
+    res.sendFile('index.html', { root: 'dist' });
 });
 // Dev task
 gulp.task('dev', function() {
-  // Start webserver
-  server.listen(serverport);
-  // Start live reload
-  lrserver.listen(livereloadport);
-  // Run the watch task, to keep taps on changes
-  gulp.run('watch');
+    // Start webserver
+    server.listen(serverport);
+    // Start live reload
+    lrserver.listen(livereloadport);
+    // Run the watch task, to keep taps on changes
+    gulp.run('watch');
 });
 
-var sass = require('gulp-sass');
-// Not necessary, but I like this one, it automatically adds prefixes for all browsers
-var autoprefixer = require('gulp-autoprefixer');
 // Styles task
 gulp.task('styles', function() {
-  gulp.src('app/styles/*.scss')
-    // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
-      .pipe(sass({onError: function(e) { console.log(e); } }))
-    // Optionally add autoprefixer
-      .pipe(autoprefixer("last 2 versions", "> 1%", "ie 8"))
-    // These last two should look familiar now :)
-      .pipe(gulp.dest('dist/css/'))
-      .pipe(refresh(lrserver));
+    var sass = require('gulp-sass');
+    // Automatically add css attribute prefixes for all browsers
+    var autoprefixer = require('gulp-autoprefixer');
+    gulp.src('app/styles/*.scss')
+        // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(refresh(lrserver));
 });
-gulp.watch(['app/styles/**/*.scss'], [
-  'styles'
-]);
+gulp.watch(['app/styles/**/*.scss'], ['styles']);
