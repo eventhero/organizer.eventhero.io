1a059cbe123006fa4932a81aaf46779486a5db24
Kliment Mamykin <kmamyk@gmail.com>
2014-09-23 16:28:35 -0400

    Using glob patterns back, removed config obect

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,30 @@
+// Generic modules
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    cache = require('gulp-cache'),
-    imagemin = require('gulp-imagemin'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
     rimraf = require('gulp-rimraf');
 
+// images plugins
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+
+// javascript plugins
+var jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    browserify = require('gulp-browserify');
+
+// css plugins
+var sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'), // Automatically add css attribute prefixes for all browsers
+    rename = require('gulp-rename'),
+    minifycss = require('gulp-minify-css');
+
+
 var config = {
-    // sources: => includes all files that contribute to compiled outcome. We watch these for changes
-    // src: => what to compile, will produce same number of files in dest
-    // dest: => dir where to place compiled files
-    css: {
-        sources: ['src/styles/**/*.scss'],
-        src: ['src/styles/*.scss'],
-        dest: 'dist/css/',
-    },
-    js: {
-        sources: ['src/scripts/*.js', 'src/scripts/**/*.js'],
-        src: ['src/scripts/*.js'],
-        dest: 'dist/js'
-    },
-    html: {
-        sources: ['src/index.html'],
-        src: ['src/index.html'],
-        dest: 'dist'
-    },
-    dist: {
-        path: 'dist',
-        glob: 'dist/**/*'
-    }
 };
 
 gulp.task('clean', function() {
-    return gulp.src(config.dist.glob, { read: false }) // much faster
+    return gulp.src('dist/**/*', { read: false }) // much faster
         .pipe(rimraf());
 });
 
@@ -44,25 +34,24 @@ gulp.task('images', function() {
         .pipe(gulp.dest('dist/images'));
 });
 
-// Browserify task
+gulp.task('html', function() {
+    return gulp.src('src/index.html')
+        // And put it in the dist folder
+        .pipe(gulp.dest('dist'))
+        .pipe(refresh(lrserver)); // Tell the lrserver to refresh
+});
+
 gulp.task('js', function() {
     // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
-    return gulp.src(config.js.src)
+    return gulp.src('src/scripts/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         // .pipe(browserify({ insertGlobals: true, debug: true }))
         .pipe(concat('main.js'))
-        .pipe(gulp.dest(config.js.dest))
+        .pipe(gulp.dest('dist/js'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
-        .pipe(gulp.dest(config.js.dest));
-});
-
-gulp.task('html', function() {
-    return gulp.src(config.html.src)
-        // And put it in the dist folder
-        .pipe(gulp.dest(config.html.dest))
-        .pipe(refresh(lrserver)); // Tell the lrserver to refresh
+        .pipe(gulp.dest('dist/js'));
 });
 
 var embedlr = require('gulp-embedlr'),
@@ -78,10 +67,10 @@ var server = express();
 // Add live reload
 server.use(livereload({port: livereloadport}));
 // Use our 'dist' folder as rootfolder
-server.use(express.static(config.dist.path));
+server.use(express.static('dist'));
 // Because I like HTML5 pushstate .. this redirects everything back to our index.html
 server.all('/*', function(req, res) {
-    res.sendFile('index.html', { root: config.dist.path });
+    res.sendFile('index.html', { root: 'dist' });
 });
 // Dev task
 gulp.task('dev', function() {
@@ -93,29 +82,26 @@ gulp.task('dev', function() {
     gulp.run('watch');
 });
 
-// Styles task
-var sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'), // Automatically add css attribute prefixes for all browsers
-    rename = require('gulp-rename'),
-    minifycss = require('gulp-minify-css');
-
 gulp.task('css', function() {
-    return gulp.src(config.css.src)
+    return gulp.src('src/styles/*.scss') // only top level scss files get compiled
         // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
         .pipe(sass({ errLogToConsole: true }))
         .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
-        .pipe(gulp.dest(config.css.dest))
+        .pipe(gulp.dest('dist/css/'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(gulp.dest(config.css.dest))
+        .pipe(gulp.dest('dist/css/'))
         .pipe(refresh(lrserver));
 
 });
-gulp.task('watch', ['clean', 'js', 'css', 'html'], function() {
+
+gulp.task('build', ['clean', 'js', 'css', 'html']);
+
+gulp.task('watch', ['build'], function() {
     // Watch our scripts
-    gulp.watch(config.js.sources, ['js']);
-    gulp.watch(config.css.sources, ['css']);
-    gulp.watch(config.html.sources, ['html']);
+    gulp.watch('src/scripts/**/*.js', ['js']);
+    gulp.watch('src/styles/**/*.scss', ['css']);
+    gulp.watch('src/index.html', ['html']);
 });
 
 gulp.task('default', ['watch']);
