d082c01847c82bf01b356c73d8ca669f86cca522
Kliment Mamykin <kmamyk@gmail.com>
2014-09-24 22:32:54 -0400

    Major rework on gulpfile.js. Separated dev and dist stages

--- gulpfile.js
+++ gulpfile.js
@@ -3,69 +3,87 @@ var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')();
 
 gulp.task('clean', function() {
-    return gulp.src('dist/*', { read: false }) // much faster
+    return gulp.src(['dev', 'dist'], { read: false }) // Nuke the whole dev and dist folders and its content
         .pipe(plugins.rimraf({ force: true }));
 });
 
-gulp.task('images', function() {
-    return gulp.src('src/images/**/*')
-        .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('dist/images'));
-});
+gulp.task('default', ['clean']);
 
-gulp.task('html', function() {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('js', function() {
-    // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
+gulp.task('dev:js', function() {
     return gulp.src('src/scripts/**/*.js')
         .pipe(plugins.jshint('.jshintrc'))
         .pipe(plugins.jshint.reporter('default'))
-        .pipe(plugins.concat('main.js'))
-        .pipe(gulp.dest('dist/js'))
-        .pipe(plugins.rename({ suffix: '.min' }))
+        .pipe(plugins.concat('scripts.js'))
+        .pipe(gulp.dest('dev/js'))
+});
+
+gulp.task('dev:css', function() {
+    return gulp.src('src/styles/*.scss') // only top level scss files get compiled, other get @included
+        .pipe(plugins.sass({ errLogToConsole: true })) // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
+        .pipe(plugins.autoprefixer({ browsers: ['last 2 versions'], cascade: false, map: false }))
+        .pipe(gulp.dest('dev/css'));
+});
+
+gulp.task('dev:images', function() {
+    return gulp.src('src/images/**/*', {})
+        .pipe(gulp.dest('dev/images'));
+});
+
+gulp.task('dev:html', function() {
+    // concat templates here
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('dev'));
+});
+
+gulp.task('dev', ['dev:images', 'dev:js', 'dev:css', 'dev:html'], function() {
+});
+
+gulp.task('dev:watch', ['dev'], function() {
+    gulp.watch('src/images/**/*', ['dev:images']);
+    gulp.watch('src/scripts/**/*.js', ['dev:js']);
+    gulp.watch('src/styles/**/*.scss', ['dev:css']);
+    gulp.watch('src/index.html', ['dev:html']);
+
+    plugins.livereload.listen();
+    gulp.watch(['dev/**']).on('change', plugins.livereload.changed);
+});
+
+gulp.task('dist:images', ['dev:images'], function() {
+    return gulp.src('dev/images/**/*')
+        .pipe(plugins.rev())
+        //.pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(plugins.rev.manifest())
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('dist:js', ['dev:js'], function() {
+    return gulp.src('dev/js/**/*.js')
+        .pipe(plugins.rev())
         .pipe(plugins.ngmin())
         .pipe(plugins.uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(plugins.rev.manifest())
         .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('css', function() {
-    return gulp.src('src/styles/*.scss') // only top level scss files get compiled
-        .pipe(plugins.sass({ errLogToConsole: true })) // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
-        .pipe(plugins.autoprefixer({ browsers: ['last 2 versions'], cascade: false, map: false }))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(plugins.rename({ suffix: '.min' }))
+gulp.task('dist:css', ['dev:css'], function() {
+    return gulp.src('dev/css/*.css')
+        .pipe(plugins.rev())
         .pipe(plugins.minifyCss())
-        .pipe(gulp.dest('dist/css/'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(plugins.rev.manifest())
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('rev', ['images', 'css', 'js', 'html'], function() {
-    // potentially move prod minifying/uglifying/rev here, and leave css/js tasks just to produce .css/.js files for dev
-    return gulp.src(['dist/**/*.css', 'dist/**/*.js'])
-        .pipe(plugins.rev())
-        .pipe(gulp.dest('dist'))
-        .pipe(plugins.rev.manifest())
+gulp.task('dist:html', ['dist:images', 'dist:js', 'dist:css', 'dev:html'], function() {
+    return gulp.src(['dist/**/rev-manifest.json', 'dev/**/*.html'])
+        .pipe(plugins.revCollector())
         .pipe(gulp.dest('dist'));
 });
 
-// clean is a dependency, and will be run before anything else
-gulp.task('build', ['rev'], function() {
-    // These tasks will be executed in parallel
-    //return gulp.start('images', 'css', 'js', 'html');
+gulp.task('dist', ['dist:html'], function() {
 });
-
-gulp.task('watch', ['build'], function() {
-    gulp.watch('src/scripts/**/*.js', ['js']);
-    gulp.watch('src/styles/**/*.scss', ['css']);
-    gulp.watch('src/index.html', ['html']);
-
-    plugins.livereload.listen();
-    gulp.watch(['dist/**']).on('change', plugins.livereload.changed);
-});
-
-gulp.task('default', ['clean']);
 
 // http://www.artandlogic.com/blog/2014/05/error-handling-in-gulp/
 // To debug either the stream pipe to debug
